735db97814cb29e8e627781a04d20706d829d96c
Mohanraj38659 <161623451+Mohanraj38659@users.noreply.github.com>
2025-02-02 18:42:10 -0800

    Create vehicle.py

---
+++ vehicle.py
@@ -0,0 +1,8 @@
+class Vehicle:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def display_info(self):
+        return f"{self.year} {self.make} {self.model}"
